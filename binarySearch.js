9ee0ff2189a851882d5b1f32b9c5374e0592f144
sunhenderson <39842892+sunh2020@users.noreply.github.com>
2022-01-04 13:48:49 -0800

    meesage

--- binarySearch.js
+++ binarySearch.js
@@ -4,22 +4,23 @@
  * @return {number}
  */
  var search = function(nums, target) {
+     // set variable first number and last number
     let first = 0;
     let last = nums.length - 1
     
-    while (first <= last) {
-        let mid = Math.floor(((first + last) / 2))
-        if(nums[mid] < target) {
-            first = mid + 1
+    while (first <= last) {    // while first is less and equal to last
+        let mid = Math.floor(((first + last) / 2))   // middle number is the largest integer value that is less than or equal to a number of (first number + last number) /2
+        if(nums[mid] < target) {      // if array of mid number is less than target 
+            first = mid + 1           // then first number is equal to middle number + 1 (move to right side of middle)
         }
-        else if(nums[mid] > target) {
-            last = mid - 1
+        else if(nums[mid] > target) {  // else if array of mid number is bigger than target
+            last = mid - 1             // then last number is equal to middle number - 1 (move to left sift of middle)
         }
         else {
-            return mid
+            return mid   // else, return middle number
         }
         }
-    return -1;
+    return -1;           // if not met all, then return -1
         
  };
 
