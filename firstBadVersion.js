d9acd557a313a43b521b514d769e4460355cda5f
sunhenderson <39842892+sunh2020@users.noreply.github.com>
2022-01-06 20:01:38 -0800

    update

--- firstBadVersion.js
+++ firstBadVersion.js
@@ -36,3 +36,4 @@
     };
 };
 
+console.log(solution[(1,2,3,4,5)] ,5);
---
+++ searchInsertPosition.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+                                                             // nums = [1,3,5,6], target = 5 > output: 2 means [2] = 5 
+    let start = 0, end = nums.length                       // start number 0 means start with index 0 which is number 1
+                                                            // end number is nums.lenght which means 4 because there are 4 numbers in array
+                                                             
+    while(start < end) {                                    // while start is smaller than end number   0 < 4
+        const mid = start + Math.floor((end - start) / 2)    // middle number is start + ((end - start) / 2) 
+        if(nums[mid]===target) {                             // if middle middle number is exactly same as target   
+           return mid                                        // return middle number
+        } else if(nums[mid] > target){                       // else if middle number is bigger than target      
+            end = mid                                      // then end number is equal to middle number 
+        } else {                                             // else start number equal to middle number + 1
+            start = mid + 1
+        }
+    }
+    return start                                              // if not met all, return start number   
+};
+
+
+console.log(searchInsert([1,3,5,6],5));
